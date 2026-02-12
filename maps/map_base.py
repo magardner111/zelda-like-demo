e615b8824ca458ac4078785a0af8166cca9a5783
Validayne <darune@gmail.com>
2026-02-11 22:47:43 -0500

    Remove enemies when they run out of health

--- maps/map_base.py
+++ maps/map_base.py
@@ -5,6 +5,7 @@ class MapBase:
     def update(self, dt, player):
         for enemy in self.enemies:
             enemy.update(dt, player)
+        self.enemies = [e for e in self.enemies if e.health > 0]
 
     def draw(self, screen, camera):
         for enemy in self.enemies:
